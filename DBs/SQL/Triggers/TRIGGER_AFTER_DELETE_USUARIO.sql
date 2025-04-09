1bda435d37016378201df6bba0cb6d53dce99afb
des-arrosho <arroyo210504@gmail.com>
2025-04-08 22:42:34 -0600

    add: Se agregaron los TRIGGERS_TRANSACCION_AFTER_(INSER, UPDATE, DELETE) dentro de la carpeta DBs/SQL/Triggers

---
+++ DBs/SQL/Triggers/TRIGGER_AFTER_DELETE_USUARIO.sql
@@ -0,0 +1,14 @@
+CREATE DEFINER=`root`@`localhost` TRIGGER `tbb_usuarios_AFTER_DELETE` AFTER DELETE ON `tbb_usuarios` FOR EACH ROW BEGIN
+    DECLARE v_usuario_id INT;
+
+    -- Seleccionar aleatoriamente un usuario con rol id = 1
+    SELECT Usuario_ID INTO v_usuario_id
+    FROM tbd_usuarios_roles
+    WHERE Rol_ID = 1
+    ORDER BY RAND()
+    LIMIT 1;
+
+    -- Insertar en la tabla bitacora
+    INSERT INTO tbb_bitacora (Usuario, Operacion, Tabla, Descripcion, Estatus, Fecha_Registro)
+    VALUES (v_usuario_id, 'Delete', 'tbb_usuarios', CONCAT('Eliminado usuario ID=', OLD.id), 1, NOW());
+END
